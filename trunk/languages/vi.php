d7ebe26e4c9ee6540dff08f6a0839fb26db2a45e
nkimkha@gmail.com <nkimkha@gmail.com@4592325a-6fce-11de-b87c-c58131bb9e62>
2009-07-30 09:03:53 +0000

    upload elgg 1.5

---
+++ trunk/languages/vi.php
@@ -0,0 +1,18 @@
+<?php
+
+$vietnamese = array(
+	'admin:site' => "Quản Trị Hệ Thống",
+	"vi" => "Tiếng Việt",
+	
+	// NEW STRING HERE
+	
+	'admin:statistics' => 'Thống kê',
+	'tools' => 'Công cụ',
+	'logout' => 'Đăng xuất',
+	'admin:user:label:search' => 'Tìm thành viên:',
+	// END NEW STRING
+);
+
+add_translation('vi', $vietnamese);
+
+?>
