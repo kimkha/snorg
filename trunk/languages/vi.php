93b34132d63a41a411bb3d73bbe739322b9457c0
Huyvtq@gmail.com <Huyvtq@gmail.com@4592325a-6fce-11de-b87c-c58131bb9e62>
2009-07-30 14:53:28 +0000

    dịch

--- trunk/languages/vi.php
+++ trunk/languages/vi.php
@@ -12,6 +12,7 @@ $vietnamese = array(
 	'admin:user:label:search' => 'Tìm thành viên:',
 	'register' => 'Đăng ký',
 	'systemmessages:dismiss' => 'nhấn vào để đóng lại',
+	'user:password:lost' => 'Phục hồi mật khẩu',
 	// END NEW STRING
 );
 
