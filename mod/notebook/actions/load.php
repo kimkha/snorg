94af79f7c9ce34bb9c61d6a9cfb43eeafc141166
nkimkha@gmail.com <nkimkha@gmail.com@4592325a-6fce-11de-b87c-c58131bb9e62>
2009-08-23 14:22:13 +0000

    Hoàn tất notebook. Chỉnh sửa chat

---
+++ mod/notebook/actions/load.php
@@ -0,0 +1,39 @@
+<?php
+
+	/**
+	* Notebook
+	* 
+	* All the Notebook CSS can be found here
+	* 
+	* @package notebook
+	* @author KimKha
+	*/
+	
+	gatekeeper();
+	
+	$user = get_loggedin_user();
+	
+	$notes = $user->getEntitiesFromRelationship(NOTEBOOK_RELATIONSHIP, true);
+	krsort($notes);
+	
+	$result = array();
+	$i = 0;
+	
+	foreach ($notes as $notebook) {
+		$n = $notebook->countAnnotations();
+		$annos = $notebook->getAnnotations('', $n);
+		$result[$i] = array();
+		foreach ($annos as $anno) {
+			$result[$i][$anno->name] = $anno->value;
+		}
+		$i++;
+	}
+	
+	header("Content-Type: application/json; charset=UTF-8");
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+	
+	echo json_encode($result);
+	
+?>
