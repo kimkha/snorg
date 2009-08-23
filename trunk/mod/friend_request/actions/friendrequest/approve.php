ef1d8a67c406fccbaaaa7857372b4986f16b1a82
bkit06 <bkit06@4592325a-6fce-11de-b87c-c58131bb9e62>
2009-08-23 11:19:29 +0000

    Thêm chức năng notification khi approve một request

--- trunk/mod/friend_request/actions/friendrequest/approve.php
+++ trunk/mod/friend_request/actions/friendrequest/approve.php
@@ -12,17 +12,42 @@ if(!$friend = get_entity(get_input("guid",0))) {
 if(remove_entity_relationship($friend->guid, 'friendrequest', $user->guid)) {
 	if(isset($CONFIG->events['create']['friend'])) {
 		$oldEventHander = $CONFIG->events['create']['friend'];
-		$CONFIG->events['create']['friend'] = array();			//Removes any event handlers
+		$CONFIG->events['create']['friend'] = array();		//Removes any event handlers
 	}
 	
 	$_SESSION['user']->addFriend($friend->guid);
-	$friend->addFriend($_SESSION['user']->guid);			//Friends mean reciprical...
+	$friend->addFriend($_SESSION['user']->guid); //Friends mean reciprical...
+	
+	$approve = new ElggObject();
+	$approve->title = "hoai";
+	$approve->dserciption ="hoai";
+	$approve->subtype = "approve";
+	$approve->owner_guid = $user->getGUID();
+	$approve->save();			
 	
 	if(isset($CONFIG->events['create']['friend'])) {
 		$CONFIG->events['create']['friend'] = $oldEventHander;
-	}
+	}		
+	
+  	
+	// send notify to friend when approve
+			
+	if (notify_user($friend->getGUID(),$user->getGUID(), "approve" , sprintf(elgg_echo('approve:content'),$friend->name, $user->name , $user->name, $user->name,$user->name) , NULL))	
+		system_message(sprintf(elgg_echo('approve:system:notify'),$friend->name));						
+	else
+		register_error("can not approve");
+	
+	
+	
 	
 	system_message(sprintf(elgg_echo('friendrequest:successful'), $friend->name));
+	
+	//echo "<pre>"; print_r($user); die;
+	
+		
+	
+	
+	
 } else {
 	system_message(sprintf(elgg_echo('friendrequest:approvefail'), $friend->name));
 }
