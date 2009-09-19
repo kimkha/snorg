503eb7d82a4bad0fbd8c7bd05e8bbf622d37b921
nkimkha@gmail.com <nkimkha@gmail.com@4592325a-6fce-11de-b87c-c58131bb9e62>
2009-09-19 02:35:12 +0000

    Thêm tính năng edit commend cho stick

--- trunk/mod/stick/actions/editcommend.php
+++ trunk/mod/stick/actions/editcommend.php
@@ -10,7 +10,26 @@
 	 * @link http://elgg.com/
 	 */
 	
-	$userId = (int) get_input('userid');
+	$id = (int) get_input('id', 0);
+	if ($id == 0) {
+		register_error(elgg_echo("stick:user:emptyid"));
+		forward($_SERVER['HTTP_REFERER']);
+	}
+	
+	$entity = get_entity($id);
+	
+	$userId = $entity->owner_guid;
 	$user = get_entity($userId);
 	
+	$title = "Edit commend ".$user->name;
+	$header = elgg_view_title($title);
+	
+	$block = elgg_view_entity($user);
+	
+	$body = elgg_view("stick/forms/commend", array("userid" => $userId, 'entity' => $entity));
+	
+	$content = elgg_view_layout("two_column_left_sidebar", $block, $header.$body);
+	
+	page_draw($title,$content);
+	exit();
 ?>
