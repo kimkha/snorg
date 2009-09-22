afbd0153261a69bd6b7ce6f17c86f5d648c8ee66
nkimkha@gmail.com <nkimkha@gmail.com@4592325a-6fce-11de-b87c-c58131bb9e62>
2009-09-22 17:43:45 +0000

    Thêm mutualfriend vào leftbar cuar profile, Fix lỗi stick trên menu profile

--- mod/friends/views/default/friends/css.php
+++ mod/friends/views/default/friends/css.php
@@ -10,3 +10,12 @@
 	float:left;
 	margin:0 5px 5px 0;
 }
+#muturalfriends-leftbar .list {
+	margin:0 10px;
+	display:table;
+	padding:8px 0 4px 8px;
+}
+#muturalfriends-leftbar .singlefriend {
+	float:left;
+	margin:0 5px 5px 0;
+}
