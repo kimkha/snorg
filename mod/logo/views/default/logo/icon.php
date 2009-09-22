32523c1078e18551d4d408b5dd2aca9a604f8f83
bkit06 <bkit06@4592325a-6fce-11de-b87c-c58131bb9e62>
2009-09-22 08:57:13 +0000

    Logo Plugin hoan chinh - chua co Lang

---
+++ mod/logo/views/default/logo/icon.php
@@ -0,0 +1,48 @@
+<?php
+	/**
+	 * Elgg file icons.
+	 * Displays an icon, depending on its mime type, for a file. 
+	 * Optionally you can specify a size.
+	 * 
+	 * @package ElggFile
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008-2009
+	 * @link http://elgg.com/
+	 */
+
+	global $CONFIG;
+	
+	$mime = $vars['mimetype'];
+	if (isset($vars['thumbnail'])) {
+		$thumbnail = $vars['thumbnail'];
+	} else {
+		$thumbnail = false;
+	}
+	
+	$size = $vars['size'];
+	if ($size != 'large') {
+		$size = 'small';
+	}
+	
+	// Handle 
+	switch ($mime)
+	{
+		case 'image/jpg' 	:
+		case 'image/jpeg' 	:
+		case 'image/pjpeg' 	:
+		case 'image/png' 	:
+		case 'image/gif' 	:
+		case 'image/bmp' 	: 
+			//$file = get_entity($file_guid);
+			if ($thumbnail) {
+				if ($size == 'small') {
+					echo "<img src=\"{$vars['url']}action/logo/icon?file_guid={$vars['file_guid']}\" border=\"0\" /> ";
+				} else {
+					echo "<img src=\"{$vars['url']}mod/logo/thumbnail.php?size=large&file_guid={$vars['file_guid']}\" border=\"0\" /> ";
+				}
+				
+			}
+		break;
+	}
+
+?>
